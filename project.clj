6f2ad6b4836cee5ca6d27959deb4ffe5951ea427
George Kontsevich <geokon@qq.com>
2019-07-25 08:58:09 +0800

    Update: Clojure version to 1.10.0

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-
 (defproject linearsystems-part2 "0.1.0-SNAPSHOT"
   :description "linea-systems in Clojure"
   :url "http://geokon-gh.github.io/linearsystems-part2/index.html"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [
-                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojure "1.10.0"]
                  [net.mikera/core.matrix "0.62.0"]]
   :main ^:skip-aot linearsystems-part2.core
   :target-path "target/%s"
