ebcfa5ebc0595c4357cb40c31d61c21d176189c2
George Kontsevich <geokon@qq.com>
2019-07-28 19:11:02 +0800

    Update: Rewrite tail recursive Hessenberg reduction

--- src/morelinear/core.clj
+++ src/morelinear/core.clj
@@ -29,7 +29,8 @@
      (elementary-reflector vector-orthogonal-to-reflection-plane))))
 
 (defn first-column-reflector
-  "Build a matrix that will reflect the INPUT-MATRIX on to the first elementary vector [ 1 0 0 .. 0 ]"
+  "Build a matrix that will reflect the first column of INPUT-MATRIX 
+  on to the first elementary vector [ 1 0 0 .. 0 ]"
   [input-matrix]
   (elementary-coordinate-reflector (get-column input-matrix
 					       0)
@@ -76,42 +77,43 @@
   (householder-reduce-to-QR (identity-matrix (row-count input-matrix))
 			    input-matrix))
 
-(defn hessenberg-form-first-partial-reflector
-  "Builds a matrix that will reduce the first column of INPUT-MATRIX to  Hessenberg Form"
+(defn first-subcolumn-reflector
+  "Build a matrix that will reflect the first column of INPUT-MATRIX 
+  on to the first elementary vector [ 1 0 0 .. 0 ]"
   [input-matrix]
-  (if
-      ;; Degenerate Case: 1 x 1 matrix
-      (or (= (column-count input-matrix) 1) (= (row-count input-matrix) 1))
-    [[ 1 ]]
-  (let [first-column (get-column input-matrix 0)
-        subdiagonal-column (subvector first-column 1 (dec (row-count first-column)))
-        orthogonal-reducer (first-column-reflector subdiagonal-column)]
-    (raise-rank orthogonal-reducer))))
+  (let [dimension (dec (dimension-count input-matrix
+					0))]
+    (block-diagonal-matrix [[[1.0]]
+			    (elementary-coordinate-reflector (subvector (get-column input-matrix 0)
+									1
+									dimension)
+							     (get-row (identity-matrix dimension) 0))])))
 
-(defn hessenberg-form-reduction
-  "Reduce the INPUT-MATRIX to  Hessenberg Form  - H , using reflectors - P. Result will be in the form [P^T H]"
-[input-matrix]
-(let [reflector-to-zero-out-first-column
-      (hessenberg-form-first-partial-reflector input-matrix)
-      input-matrix-with-first-column-zeroed-out
-      (mmul reflector-to-zero-out-first-column input-matrix (transpose reflector-to-zero-out-first-column))]
-  (if
-      ;; Base Case: We're out of columns/rows to reduce
-      ;;            Return the reflector and the reduced column
-      (or (= (column-count input-matrix) 1) (= (row-count input-matrix) 1))
-      [reflector-to-zero-out-first-column input-matrix-with-first-column-zeroed-out]
-      ;; Recursive step: Get the Q^{-1}R of the submatrix
-      ;;                 Then and combine it with your reflector and reduced matrix
-      (let [submatrix (submatrix
-                       input-matrix-with-first-column-zeroed-out
-                       1 (dec (row-count input-matrix))
-                       1 (dec (column-count input-matrix)))
-            [submatrix-P submatrix-H] ( hessenberg-form-reduction submatrix)]
-        [(mmul (raise-rank submatrix-P)
-               reflector-to-zero-out-first-column)
-         (raise-rank-and-insert-row-column submatrix-H
-                                   (subvector (get-column input-matrix-with-first-column-zeroed-out 0) 1 (dec (row-count input-matrix-with-first-column-zeroed-out)))
-                                   (get-row input-matrix-with-first-column-zeroed-out 0))]))))
+(defn hessenberg-reduction
+  "Increase the dimension of a reflector by padding it with an identity matrix"
+  [reduction-matrix input-matrix]
+  (if (= 2 (row-count input-matrix))
+    reduction-matrix
+    (let [reflector (first-subcolumn-reflector input-matrix)]
+      (do (assign! input-matrix
+		   (mmul reflector
+			 input-matrix
+			 reflector))
+	  (recur (mmul reduction-matrix
+		       (block-diagonal-matrix [(identity-matrix (- (row-count reduction-matrix)
+								   (row-count input-matrix)))
+					       reflector]))
+		 (submatrix input-matrix
+			    1
+			    (dec (row-count input-matrix))
+			    1
+			    (dec (column-count input-matrix))))))))
+
+(defn hessenberg
+  "A wrapper for the real function"
+  [input-matrix]
+  (hessenberg-reduction (identity-matrix (row-count input-matrix))
+		       input-matrix))
 
 (defn matrix-template
 "template"
