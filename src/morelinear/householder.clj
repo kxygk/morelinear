290ad0fa1246cfb1187b6146201d9840b060197a
George Kontsevich <geokon@qq.com>
2019-08-01 22:37:02 +0800

    Update: Move code to smaller namespaces
    For a coming reorg of the webpage and layout...

--- src/morelinear/householder.clj
+++ src/morelinear/householder.clj
@@ -1,11 +1,5 @@
-(ns morelinear.core
-  (:require [clojure.core.matrix :refer :all])  ;[denisovan.core :as den]
-  (:gen-class))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(ns morelinear.householder
+  (:require [clojure.core.matrix :refer :all]))
 
 (defn elementary-reflector
   "Build a matrix that will reflect vector across the hyperplane orthogonal to REFLECTION-AXIS"
@@ -76,46 +70,3 @@
   [input-matrix]
   (householder-reduce-to-QR (identity-matrix (row-count input-matrix))
 			    input-matrix))
-
-(defn first-subcolumn-reflector
-  "Build a matrix that will reflect the first column of INPUT-MATRIX 
-  on to the first elementary vector [ 1 0 0 .. 0 ]"
-  [input-matrix]
-  (let [dimension (dec (dimension-count input-matrix
-					0))]
-    (block-diagonal-matrix [[[1.0]]
-			    (elementary-coordinate-reflector (subvector (get-column input-matrix 0)
-									1
-									dimension)
-							     (get-row (identity-matrix dimension) 0))])))
-
-(defn hessenberg-reduction
-  "Increase the dimension of a reflector by padding it with an identity matrix"
-  [reduction-matrix input-matrix]
-  (if (= 2 (row-count input-matrix))
-    reduction-matrix
-    (let [reflector (first-subcolumn-reflector input-matrix)]
-      (do (assign! input-matrix
-		   (mmul reflector
-			 input-matrix
-			 reflector))
-	  (recur (mmul reduction-matrix
-		       (block-diagonal-matrix [(identity-matrix (- (row-count reduction-matrix)
-								   (row-count input-matrix)))
-					       reflector]))
-		 (submatrix input-matrix
-			    1
-			    (dec (row-count input-matrix))
-			    1
-			    (dec (column-count input-matrix))))))))
-
-(defn hessenberg
-  "A wrapper for the real function"
-  [input-matrix]
-  (hessenberg-reduction (identity-matrix (row-count input-matrix))
-		       input-matrix))
-
-(defn matrix-template
-"template"
-[matrix]
-)
