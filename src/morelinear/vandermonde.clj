b6b3938354a46b3f87ebb5e0f3b8f74bd8c52ea4
George Kontsevich <geokon@qq.com>
2019-08-21 20:58:07 +0800

    Added: Solving for polynomials using Vandermonde matrices

--- src/morelinear/vandermonde.clj
+++ src/morelinear/vandermonde.clj
@@ -1,5 +1,6 @@
 (ns morelinear.vandermonde
-  (:require [clojure.core.matrix :refer :all]
+  (:require [morelinear.leastsquares :as leastsquares]
+	    [clojure.core.matrix :refer :all]
 	    [clojure.core.matrix.linear :as linear]))
 (set-current-implementation :vectorz)
 
@@ -20,3 +21,50 @@
   ([polynomial-input-values]
    (polynomial-matrix polynomial-input-values
 		      (ecount polynomial-input-values))))
+
+(defn split-into-x-y
+  [points]
+  (let [vector-of-xs-and-ys (apply mapv vector points)]
+    {:x (mutable (first vector-of-xs-and-ys))
+     :y (mutable (second vector-of-xs-and-ys))}))
+
+
+(defmulti polynomial-factors
+  "Solve for a polynomial equation of DEGREE that fits the given POINTS
+  and return the list of polynomial factors.
+  the underlying least squares METHOD used can be:
+  :lu
+  :lu-jumbo
+  :householder"
+  (fn [degree
+       points
+       method]
+    method))
+
+(defmethod polynomial-factors :lu
+  [degree
+   points
+   method]
+  (let [xy (split-into-x-y points)]
+    (leastsquares/lu-direct (polynomial-matrix (:x xy)
+					       degree)
+			    (:y xy))))
+
+(defmethod polynomial-factors :lu-jumbo
+  [degree
+   points
+   method]
+  (let [xy (split-into-x-y points)]
+    (leastsquares/lu-jumbo (polynomial-matrix (:x xy)
+					      degree)
+			   (:y xy))))
+
+(defmethod polynomial-factors :householder
+  [degree
+   points
+   method]
+  (let [xy (split-into-x-y points)
+	vandermonde-matrix (mutable (polynomial-matrix (:x xy)
+						       degree))]
+    (leastsquares/householder-qr vandermonde-matrix
+				 (:y xy))))
